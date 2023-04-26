cd7d3488fece295706913af06dea4eb3013c4bdb
Gustaf Silver <gustaf.silver@sweetspot.io>
2023-04-26 13:54:31 +0200

    Update

--- zsh/scripts.sh
+++ zsh/scripts.sh
@@ -2,11 +2,24 @@
 
 docker-nuke() {
 	docker stop $(docker ps -qa); docker rm $(docker ps -qa); docker rmi -f $(docker images -qa); docker volume rm $(docker volume ls -q); docker network rm $(docker network ls -q)
+
+}
+
+code-nuke() {
+  sudo -rm -rf docker/mysql/data
+  rm -rf var
+  rm -rf vendor
+  docker-nuke
+  make build
+  make up
+  make composer
+  make refresh-db
+  docker-ip-2
 }
 
 
 docker-populate-hosts() {
-    docker_ip | sudo tee -a /etc/hosts
+    docker-ip | sudo tee -a /etc/hosts
 }
 
 
@@ -15,6 +28,23 @@ docker-ip() {
         IP=$(docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" "$ID")
         NAME=$(docker ps | grep "$ID" | awk '{print $NF}')
         printf "%s %s\n" "$IP" "$NAME"
+    done
+}
+
+docker-ip-2() {
+    # Loop through all running containers
+    for ID in $(docker ps -q); do
+        IP=$(docker inspect --format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" "$ID")
+        NAME=$(docker ps --format "{{.Names}}" | grep "$ID" | awk '{print $NF}')
+        
+        # Check if the hostname already exists in /etc/hosts
+        if grep -q "$NAME" /etc/hosts; then
+            # Replace the existing IP address with the new one
+            sudo sed -i "s/^[[:space:]]*$IP[[:space:]]*$NAME$/\t$IP\t$NAME/" /etc/hosts
+        else
+            # Append the new IP address and hostname to /etc/hosts
+            echo -e "\t$IP\t$NAME" | sudo tee -a /etc/hosts > /dev/null
+        fi
     done
 }
 
