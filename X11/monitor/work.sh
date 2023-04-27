62a721b7b8f5c6f49964f030843409b2feb780e5
Gustaf Silver <gustaf.silver@sweetspot.io>
2023-04-27 10:45:32 +0200

    Update monitors

--- X11/monitor/work.sh
+++ X11/monitor/work.sh
@@ -1,1 +1,4 @@
-xrandr --output eDP-1 --mode 1920x1200 --rate 60 --output DP-3 --mode 1920x1080 --above eDP-1
+xrandr --output eDP-1 --mode 1920x1200 --rate 60 --output DP-1 --mode 1920x1080 --above eDP-1 
+xrandr --output eDP-1 --mode 1920x1200 --rate 60 --output DP-2 --mode 1920x1080 --above eDP-1 
+xrandr --output eDP-1 --mode 1920x1200 --rate 60 --output DP-3 --mode 1920x1080 --above eDP-1 
+xrandr --output eDP-1 --mode 1920x1200 --rate 60 --output DP-4 --mode 1920x1080 --above eDP-1 
