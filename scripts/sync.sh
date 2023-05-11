4a3dccea692e8aae3458ed2f4a0545f481f24ab0
Gustaf Silver <gustaf.silver@sweetspot.io>
2023-05-11 10:28:03 +0200

    Update dotfiles

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -8,13 +8,12 @@ fi
 
 LOCAL_PATH="$1"
 REMOTE_PATH="$2"
-rclone copy "$LOCAL_PATH" "$REMOTE_PATH"
-
-rclone copy "$REMOTE_PATH" "$LOCAL_PATH"
+rclone sync "$LOCAL_PATH" "$REMOTE_PATH"
+rclone sync "$REMOTE_PATH" "$LOCAL_PATH"
 
 DIFFS=$(rclone check "$LOCAL_PATH" "$REMOTE_PATH" --quiet)
 
 if [ -n "$DIFFS" ]; then
-    echo "There are differences between the local folder and Google Drive:"
+    echo "There are differences between the local folder and remote folder"
     echo "$DIFFS"
 fi
