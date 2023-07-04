7aa22398bb9f2d3cd44a59ed9a5bad025d65db8c
Gustaf Silver <gustaf.silver@sweetspot.io>
2023-07-04 11:20:08 +0200

    Update

---
+++ scripts/monitor_work.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+xrandr --auto
+
+xset dpms force off
+
+laptop_monitor=$(xrandr | awk '/ connected/ && /eDP-/ {print $1}' | head -n 1 )
+external_monitor=$(xrandr | awk '/ connected/ && /DP-/ {print $1}' | tail -n 1 )
+
+if [ -n "$laptop_monitor" ] && [ -n "$external_monitor" ]; then
+    if xrandr --output "$laptop_monitor" --primary --mode 3840x2400 --output "$external_monitor" --mode 3840x2160 --above "$laptop_monitor"; then
+        xset dpms force on
+    else
+        echo "xrandr failed to configure the monitors."
+        exit 1
+    fi
+else
+    echo "Unable to find the specified monitors."
+    exit 1
+fi
