31aae3100abaaa82c957a34fc0f177c72d009fa6
Gustaf Silver <gustaf.silver@sweetspot.io>
2023-07-04 11:19:25 +0200

    Update

--- rofi/rofi-monitor-menu.sh
+++ rofi/rofi-monitor-menu.sh
@@ -11,12 +11,12 @@ options="$option0\n$option1\n$option2"
 selected="$(echo -e "$options" | rofi -lines 3 -dmenu -p "monitor config")"
 case $selected in
   $option0)
-    sh $DOTFILES/scripts/monitor/home.sh && sh $DOTFILES/scripts/ui/feh.sh
+    sh $DOTFILES/scripts/monitor_home.sh && sh $DOTFILES/scripts/feh.sh
     ;;
   $option1)
-    sh $DOTFILES/scripts/monitor/default.sh && sh $DOTFILES/scripts/ui/feh.sh
+    sh $DOTFILES/scripts/monitor_default.sh && sh $DOTFILES/scripts/feh.sh
     ;;
   $option2)
-    sh $DOTFILES/scripts/monitor/work.sh && sh $DOTFILES/scripts/ui/feh.sh
+    sh $DOTFILES/scripts/monitor_work.sh && sh $DOTFILES/scripts/feh.sh
     ;;
 esac
